d5e6cde11712b51adbf591ec1151829088d47ca0
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-02-12 22:07:44 +0400

    Update hw4task2_test.py

--- hw4task2_test.py
+++ hw4task2_test.py
@@ -4,17 +4,18 @@ from io import StringIO
 import Task_2
 from random import randint
 class TestPolydivisibleNumbers(unittest.TestCase):
-
-
-    @patch('builtins.input', side_effect=['123456', '789', '10203040506070809', '0'])
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_main_input(self, mock_stdout, mock_input):
-        Task_2.main()
-        expected_output = "1 % 1 = 0\n2 % 2 = 0\n3 % 3 = 0\n4 % 4 = 0\n5 % 5 = 0\n6 % 6 = 0\nTrue\n"\
-                          "7 % 1 = 0\n8 % 2 = 0\n9 % 3 = 0\nFalse\n"\
-                          "1 % 1 = 0\n0 % 2 = 0\n2 % 3 = 0\n0 % 4 = 0\n3 % 5 = 0\n0 % 6 = 0\n4 % 7 = 0\n5 % 8 = 0\n6 % 9 = 0\n7 % 10 = 0\n8 % 11 = 0\n9 % 12 = 0\nTrue\n"\
-                          "Enter a number (N>0): \nTrue\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+    def test_main_input(self):
+        test_cases = [
+                {'input': '123456', 'expected_output': '1 % 1 = 0\n12 % 2 = 0\n123 % 3 = 0\n1234 % 4 = 2\nFalse\n'},
+                {'input': '789', 'expected_output': '7 % 1 = 0\n78 % 2 = 0\n789 % 3 = 0\nTrue\n'},
+                {'input': '10203040506070809', 'expected_output': '1 % 1 = 0\n10 % 2 = 0\n102 % 3 = 0\n1020 % 4 = 0\n10203 % 5 = 3\nFalse\n'},
+                {'input': '122', 'expected_output': '1 % 1 = 0\n12 % 2 = 0\n122 % 3 = 0\nTrue\n'},
+                {'input': '0', 'expected_output': 'True\n'}
+        ]
+        for test_case in test_cases:
+            with patch('builtins.input', return_value=test_case['input']), patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+                Task_2.main()
+                self.assertEqual(mock_stdout.getvalue(), test_case['expected_output'])
 
     def test_length_num(self):
         num = str(randint(1, 1000000000))
